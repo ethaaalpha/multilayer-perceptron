4ca8680d21b0be61fc97b6d61441285055acacaf
Ethaniel Billon <ebillon@student.42lyon.fr>
2025-02-14 14:18:39 +0100

    adam optimizer

--- src/processing/functions/optimizers.py
+++ src/processing/functions/optimizers.py
@@ -60,19 +60,50 @@ class RMSprop(AbtractOptimizer):
     """RMSprop
     0 <= decay_rate <= 1
     """
-    def __init__(self, learning_rate = 0.001, decay_rate= 0.9):
+    def __init__(self, learning_rate = 0.001, decay_rate = 0.9):
         self.learning_rate = learning_rate
         self.velocity_w = 0
         self.velocity_b = 0
         self.decay_rate = decay_rate
-        self.epsilon = 1e-15
+        self.epsilon = 1e-8
 
     def getW(self, W, dW):
-        self.velocity_w = self.decay_rate * self.velocity_w + (1 - self.decay_rate)*dW**2
+        self.velocity_w = self.decay_rate * self.velocity_w + (1 - self.decay_rate) * dW**2
         return W - (self.learning_rate / (self.epsilon + np.sqrt(self.velocity_w)) * dW)
 
     def getB(self, b, dB):
-        self.velocity_b = self.decay_rate * self.velocity_b + (1 - self.decay_rate)*dB**2
+        self.velocity_b = self.decay_rate * self.velocity_b + (1 - self.decay_rate) * dB**2
         return b - (self.learning_rate / (self.epsilon + np.sqrt(self.velocity_b)) * dB)
 
-    pass
+class Adam(AbtractOptimizer):
+    """Adam"""
+
+    def __init__(self, learning_rate = 0.001, beta_1 = 0.9, beta_2 = 0.999):
+        self.learning_rate = learning_rate
+        self.velocity_w = 0
+        self.velocity_b = 0
+        self.momentum_w = 0
+        self.momentum_b = 0
+        self.beta_1 = beta_1
+        self.beta_2 = beta_2
+        self.epsilon = 1e-8
+        self.time_w = 0
+        self.time_b = 0
+
+    def getW(self, W, dW):
+        self.time_w += 1
+        self.momentum_w = self.beta_1 * self.momentum_w + (1 - self.beta_1) * dW
+        self.velocity_w = self.beta_2 * self.velocity_w + (1 - self.beta_2) * dW**2
+
+        corrected_momentum = self.momentum_w / (1 - self.beta_1 ** self.time_w)
+        corrected_velocity = self.velocity_w / (1 - self.beta_2 ** self.time_w)
+        return W - (self.learning_rate / (self.epsilon + np.sqrt(corrected_velocity)) * corrected_momentum)
+
+    def getB(self, b, dB):
+        self.time_b += 1
+        self.momentum_b = self.beta_1 * self.momentum_b + (1 - self.beta_1) * dB
+        self.velocity_b = self.beta_2 * self.velocity_b + (1 - self.beta_2) * dB**2
+
+        corrected_momentum = self.momentum_b / (1 - self.beta_1 ** self.time_b)
+        corrected_velocity = self.velocity_b / (1 - self.beta_2 ** self.time_b)
+        return b - (self.learning_rate / (self.epsilon + np.sqrt(corrected_velocity)) * corrected_momentum)
--- src/train.py
+++ src/train.py
@@ -5,7 +5,7 @@ from preprocessing.encoders import HotEncoder
 from processing.multilayer import MultiLayer, ModelConfiguration
 from processing.tools.model_manager import ModelManager
 from processing.functions.activators import Sigmoide, SoftMax
-from processing.functions.optimizers import SGD, SGDNesterovMomentum, RMSprop
+from processing.functions.optimizers import SGD, SGDNesterovMomentum, RMSprop, Adam
 from processing.functions.initializers import He_Uniform
 from processing.functions.losses import *
 
@@ -18,14 +18,14 @@ def main():
     validation_data = scaler.scale_data(validation_dataset, HotEncoder())
 
     lr = 0.001
-    config = ModelConfiguration(number_epoch=60, loss=BCE())
+    config = ModelConfiguration(number_epoch=40, loss=BCE())
 
     mlp: MultiLayer = MultiLayer(training_data[0], training_data[1], config)
     mlp.add_input_layer(30)
-    mlp.add_dense_layer(24, activator=Sigmoide(), initializer=He_Uniform(), optimizer=RMSprop(lr))
-    mlp.add_dense_layer(24, activator=Sigmoide(), initializer=He_Uniform(), optimizer=RMSprop(lr))
-    mlp.add_dense_layer(24, activator=Sigmoide(), initializer=He_Uniform(), optimizer=RMSprop(lr))
-    mlp.add_output_layer(2, activator=SoftMax(), initializer=He_Uniform(), optimizer=RMSprop(lr))
+    mlp.add_dense_layer(24, activator=Sigmoide(), initializer=He_Uniform(), optimizer=Adam(lr))
+    mlp.add_dense_layer(24, activator=Sigmoide(), initializer=He_Uniform(), optimizer=Adam(lr))
+    mlp.add_dense_layer(24, activator=Sigmoide(), initializer=He_Uniform(), optimizer=Adam(lr))
+    mlp.add_output_layer(2, activator=SoftMax(), initializer=He_Uniform(), optimizer=Adam(lr))
     mlp.learn(validation_data)
 
     ModelManager.export_model(mlp, "model.json")
